4c878621cd3f6e0ee0a51207ec0ac1a26e539b20
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 23:50:20 +0300

    feat: added delete note command with its handler

---
+++ src/CleanArch.Application/Commands/Note/DeleteNote/DeleteNoteCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Domain.Common;
+using CleanArch.Domain.Constants;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Note.DeleteNote
+{
+    public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommandRequest, AppResponse>
+    {
+        private INoteRepository noteRepository;
+        public DeleteNoteCommandHandler(INoteRepository noteRepository)
+        {
+            this.noteRepository = noteRepository;
+        }
+        public async Task<AppResponse> Handle(DeleteNoteCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await noteRepository.RemoveAsync(request.Id);
+            return result == null
+                ? new ErrorResponse(Messages.ERROR_OCCURRED)
+                : new SuccessResponse(Messages.DELETED_NOTE_PERMANENTLY);
+        }
+    }
+}
---
+++ src/CleanArch.Application/Commands/Note/DeleteNote/DeleteNoteCommandRequest.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Note.DeleteNote
+{
+    public class DeleteNoteCommandRequest: IRequest<AppResponse>
+    {
+        public Guid Id { get; set; }
+
+        public DeleteNoteCommandRequest(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
