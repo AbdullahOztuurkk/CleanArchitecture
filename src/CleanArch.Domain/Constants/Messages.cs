32a7a71b2c680bb98760dbf2268546e5844f0d26
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 23:26:30 +0300

    feat: added messages class for response structure

---
+++ src/CleanArch.Domain/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Constants
+{
+    public static class Messages
+    {
+        public static string CREATED_NOTE_SUCCESSFULLY = "Note has been added successfully";
+        public static string DELETED_NOTE_PERMANENTLY = "Note has been deleted permanently";
+
+        public static string CREATED_TAG_SUCCESSFULLY = "Tag has been added successfully";
+        public static string DELETED_TAG_PERMANENTLY = "Tag has been deleted permanently";
+
+        public static string ERROR_OCCURRED = "Error occurred. Please try again later!";
+
+    }
+}
