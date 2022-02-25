70c13af0ff4c17a3e26f0bf3b42b32aeeb5e6475
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 22:38:45 +0300

    feat: global profile has been added for mapping operations

---
+++ src/CleanArch.Application/Mapping/GeneralProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArch.Application.Commands.Note.CreateNote;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.Mapping
+{
+    public class GeneralProfile:Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<Note, CreateNoteCommandRequest>()
+                .ReverseMap();
+        }
+    }
+}
