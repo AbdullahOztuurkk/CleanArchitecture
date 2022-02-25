acfbfd2cf1983d6b65bab0f86224481caf891ac3
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 22:40:19 +0300

    feat: created generic class for response of any request

---
+++ src/CleanArch.Domain/Common/AppResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Common
+{
+    public class AppResponse
+    {
+        public bool IsSucceed { get; internal set; }
+        public string Message { get; internal set; }
+
+        public AppResponse(bool isSucceed, string message)
+        {
+            IsSucceed = isSucceed;
+            Message = message;
+        }
+    }
+    
+    public class ErrorResponse : AppResponse
+    {
+        public ErrorResponse(string Message) : base(false, Message) { }
+    }
+
+    public class SuccessResponse : AppResponse
+    {
+        public SuccessResponse(string Message) : base(true, Message) { }
+    }
+}
