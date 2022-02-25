ca6af083b8675addd8611ceb83b8a64fcb1062ac
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 22:41:34 +0300

    feat: added create note command with its handler

---
+++ src/CleanArch.Application/Commands/Note/CreateNote/CreateNoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Note.CreateNote
+{
+    public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommandRequest,AppResponse>
+    {
+        private INoteRepository noteRepository;
+        private IMapper mapper;
+        public CreateNoteCommandHandler(IMapper mapper, INoteRepository noteRepository)
+        {
+            this.mapper = mapper;
+            this.noteRepository = noteRepository;
+        }
+
+        public async Task<AppResponse> Handle(CreateNoteCommandRequest request, CancellationToken cancellationToken)
+        {
+            var note = mapper.Map<Domain.Entities.Note>(request);
+            var result = await noteRepository.AddAsync(note);
+            return result == null 
+                ? new ErrorResponse("omas") 
+                : new SuccessResponse("olur");
+        }
+    }
+}
---
+++ src/CleanArch.Application/Commands/Note/CreateNote/CreateNoteCommandRequest.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Note.CreateNote
+{
+    public class CreateNoteCommandRequest : IRequest<AppResponse>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public CreateNoteCommandRequest(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+}
