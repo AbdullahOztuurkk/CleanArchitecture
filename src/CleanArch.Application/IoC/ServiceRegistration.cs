e95df1e41a941dd041fe7338392a33bdf40f34ca
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-25 19:00:23 +0300

    feat: added extension method of application services.

---
+++ src/CleanArch.Application/IoC/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArch.Application.IoC
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
