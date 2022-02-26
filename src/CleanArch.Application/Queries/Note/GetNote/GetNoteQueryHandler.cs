5d45c4505a6e1a4d4b50adb60b4a3371391d809f
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-27 01:40:06 +0300

    feat: get note query has been created temporarily with its handler

---
+++ src/CleanArch.Application/Queries/Note/GetNote/GetNoteQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Queries.Note.GetNote
+{
+    public class GetNoteQueryHandler : IRequestHandler<GetNoteQueryRequest, GetNoteQueryResponse>
+    {
+        private readonly INoteRepository noteRepository;
+        public GetNoteQueryHandler(INoteRepository noteRepository)
+        {
+            this.noteRepository = noteRepository;
+        }
+        public Task<GetNoteQueryResponse> Handle(GetNoteQueryRequest request, CancellationToken cancellationToken)
+        {
+            //TODO: Must be refactor after created concrete repository class
+            return null;
+        }
+    }
+}
