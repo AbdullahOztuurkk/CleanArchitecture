1f988e3c824d118f1e167349d354803fe3a32efb
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-28 00:47:41 +0300

    feat: Unit Of Work interface has been created

---
+++ src/CleanArch.Application/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveAsync();
+        public INoteRepository noteRepository { get; set; }
+        public ITagRepository tagRepository { get; set; }
+    }
+}
