f49d727b72befdda42c4ff9cca0164a40c3010a4
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-02 00:53:23 +0300

    feat: created unit of work class

---
+++ src/CleanArch.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Application.Interfaces.UnitOfWork;
+using CleanArch.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Persistence.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(
+            CleanArchContext context,
+            INoteRepository NoteRepository,
+            ITagRepository TagRepository)
+        {
+            Context = context;
+            this.NoteRepository = NoteRepository;
+            this.TagRepository = TagRepository;
+        }
+        public INoteRepository NoteRepository { get; }
+        public ITagRepository TagRepository { get; }
+        public CleanArchContext Context { get; }
+
+        public async Task<int> SaveAsync()
+        {
+            return await Context.SaveChangesAsync();
+        }
+    }
+}
