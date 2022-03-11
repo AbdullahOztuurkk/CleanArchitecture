b7109b1e6bf511621e8c5e0d3f7681b4a07b16d6
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-11 12:18:01 +0300

    feat: dbcontext has been added

---
+++ src/CleanArch.Persistence/Context/CleanArchContext.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces.Context;
+using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Persistence.Context
+{
+    public class CleanArchContext : DbContext, IApplicationDbContext
+    {
+        public CleanArchContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public CleanArchContext()
+        {
+            //Needs for Entity Framework
+        }
+
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
