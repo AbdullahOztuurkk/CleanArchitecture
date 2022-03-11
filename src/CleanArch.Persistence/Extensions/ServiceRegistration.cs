0106916f363415760361d27ce8c0b1a1fb3f29fe
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-11 12:22:13 +0300

    feat: added extension method about persistence services

---
+++ src/CleanArch.Persistence/Extensions/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Persistence.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CleanArch.Persistence.Repositories;
+using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Application.Interfaces.UnitOfWork;
+
+namespace CleanArch.Persistence.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<CleanArchContext>(options => options.UseSqlServer(configuration.GetConnectionString("CleanArchContext")));
+            services.AddScoped<INoteRepository, NoteRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+        }
+    }
+}
