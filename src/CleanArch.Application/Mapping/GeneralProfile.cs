b0ee0ea50606c02a621ee6752a168c7ac50b966d
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-15 19:17:01 +0300

    feat: mapping profile has been updated

--- src/CleanArch.Application/Mapping/GeneralProfile.cs
+++ src/CleanArch.Application/Mapping/GeneralProfile.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using CleanArch.Application.Commands.Note.CreateNote;
 using CleanArch.Application.Commands.Note.DeleteNote;
+using CleanArch.Application.Commands.Tag.CreateTag;
+using CleanArch.Application.Commands.Tag.DeleteTag;
 using CleanArch.Application.Queries.Note.GetAllNote;
 using CleanArch.Application.Queries.Note.GetNote;
+using CleanArch.Application.Queries.Tag.GetAllTag;
+using CleanArch.Application.Queries.Tag.GetTag;
 using CleanArch.Domain.Entities;
 
 namespace CleanArch.Application.Mapping
@@ -16,11 +20,19 @@ namespace CleanArch.Application.Mapping
                 .ReverseMap();
             CreateMap<Note, DeleteNoteCommandRequest>()
                 .ReverseMap();
+            CreateMap<Tag, CreateTagCommandRequest>()
+                .ReverseMap();
+            CreateMap<Tag, DeleteTagCommandRequest>()
+                .ReverseMap();
 
             //Queries
             CreateMap<Note, GetAllNoteQueryResponse>()
                 .ReverseMap();
             CreateMap<Note, GetNoteQueryResponse>()
+                .ReverseMap();
+            CreateMap<Tag, GetAllTagQueryResponse>()
+                .ReverseMap();
+            CreateMap<Tag, GetTagQueryResponse>()
                 .ReverseMap();
         }
     }
