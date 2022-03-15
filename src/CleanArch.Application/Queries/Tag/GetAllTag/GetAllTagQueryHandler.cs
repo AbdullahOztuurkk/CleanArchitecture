ef066ae3786cced2e6935cbeef42ba32a7caf46e
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-15 16:47:32 +0300

    feat: getall tag handler class has been added with its response and request model class

---
+++ src/CleanArch.Application/Queries/Tag/GetAllTag/GetAllTagQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CleanArch.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Queries.Tag.GetAllTag
+{
+    public class GetAllTagQueryHandler : IRequestHandler<GetAllTagQueryRequest, List<GetAllTagQueryResponse>>
+    {
+        private readonly ITagRepository tagRepository;
+        private readonly IMapper mapper;
+        public GetAllTagQueryHandler(ITagRepository tagRepository, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.tagRepository = tagRepository;
+        }
+        public async Task<List<GetAllTagQueryResponse>> Handle(GetAllTagQueryRequest request, CancellationToken cancellationToken)
+        {
+            var result = await tagRepository.GetAsync();
+            return mapper.Map<List<GetAllTagQueryResponse>>(result);
+        }
+    }
+}
---
+++ src/CleanArch.Application/Queries/Tag/GetAllTag/GetAllTagQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Queries.Tag.GetAllTag
+{
+    public class GetAllTagQueryRequest:IRequest<List<GetAllTagQueryResponse>>
+    {
+
+    }
+}
