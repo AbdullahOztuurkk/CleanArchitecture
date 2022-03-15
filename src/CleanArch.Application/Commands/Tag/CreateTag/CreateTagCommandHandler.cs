89323b749ddf9ca1119d23da0c758d145b1fa198
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-15 17:04:19 +0300

    feat: create tag handler class has been created with its response and request model classes

---
+++ src/CleanArch.Application/Commands/Tag/CreateTag/CreateTagCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Domain.Common;
+using CleanArch.Domain.Constants;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Tag.CreateTag
+{
+    public class CreateTagCommandHandler : IRequestHandler<CreateTagCommandRequest, AppResponse>
+    {
+        private ITagRepository tagRepository;
+        private IMapper mapper;
+        public CreateTagCommandHandler(IMapper mapper, ITagRepository tagRepository)
+        {
+            this.mapper = mapper;
+            this.tagRepository = tagRepository;
+        }
+
+        public async Task<AppResponse> Handle(CreateTagCommandRequest request, CancellationToken cancellationToken)
+        {
+            var tag = mapper.Map<Domain.Entities.Tag>(request);
+            var result = await tagRepository.AddAsync(tag);
+            return result == null
+                ? new ErrorResponse(Messages.ERROR_OCCURRED)
+                : new SuccessResponse(Messages.CREATED_TAG_SUCCESSFULLY);
+        }
+    }
+}
---
+++ src/CleanArch.Application/Commands/Tag/CreateTag/CreateTagCommandRequest.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Tag.CreateTag
+{
+    public class CreateTagCommandRequest:IRequest<AppResponse>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public CreateTagCommandRequest(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
