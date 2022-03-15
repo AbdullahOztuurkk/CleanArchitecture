6dffffd97cd25aaf718b6932f0e2eb987c8aa395
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-15 17:09:08 +0300

    feat: delete tag handler class has been created with its response and request model classes

---
+++ src/CleanArch.Application/Commands/Tag/DeleteTag/DeleteTagCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CleanArch.Application.Interfaces.Repositories;
+using CleanArch.Domain.Common;
+using CleanArch.Domain.Constants;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Commands.Tag.DeleteTag
+{
+    public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommandRequest, AppResponse>
+    {
+        private readonly ITagRepository tagRepository;
+        private readonly IMapper mapper;
+        public DeleteTagCommandHandler(ITagRepository tagRepository, IMapper mapper)
+        {
+            this.tagRepository = tagRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<AppResponse> Handle(DeleteTagCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await tagRepository.RemoveAsync(request.Id);
+            return result == null
+                ? new ErrorResponse(Messages.ERROR_OCCURRED)
+                : new SuccessResponse(Messages.DELETED_NOTE_PERMANENTLY);
+        }
+    }
+}
