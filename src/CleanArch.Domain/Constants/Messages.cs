a9d868cd6abdf82fb12b1798e49130b8fd88a9b4
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-23 16:30:13 +0300

    feat: added information messages

--- src/CleanArch.Domain/Constants/Messages.cs
+++ src/CleanArch.Domain/Constants/Messages.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanArch.Domain.Constants
+﻿namespace CleanArch.Domain.Constants
 {
     public static class Messages
     {
-        public const string CREATED_NOTE_SUCCESSFULLY = "Note has been added successfully";
+        public const string CREATED_NOTE_SUCCESSFULLY = "Note has been added!";
         public const string DELETED_NOTE_PERMANENTLY = "Note has been deleted permanently";
 
-        public const string CREATED_TAG_SUCCESSFULLY = "Tag has been added successfully";
+        public const string CREATED_TAG_SUCCESSFULLY = "Tag has been added!";
         public const string DELETED_TAG_PERMANENTLY = "Tag has been deleted permanently";
 
         public const string ERROR_OCCURRED = "Error occurred. Please try again later!";
         public const string VALIDATION_ERROR = "Some values are not valid. Please try again.";
 
+        public const string UPDATED_NOTE_SUCCESSFULLY = "Note has been updated!";
     }
 }
