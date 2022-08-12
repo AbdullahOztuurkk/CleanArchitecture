14050a7e6183945a21200f1fd42d47ab8e827655
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-08-12 10:26:54 +0300

    feat: tag validator has been added for update operation

---
+++ src/CleanArch.Application/Validations/UpdateEvent/UpdateTagValidator.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.Features.Commands.UpdateEvent;
+using CleanArch.Domain.Constants;
+using FluentValidation;
+
+namespace CleanArch.Application.Validations.UpdateEvent
+{
+    public class UpdateTagValidator : AbstractValidator<UpdateTagCommandRequest>
+    {
+        public UpdateTagValidator()
+        {
+            RuleFor(pred => pred.Description)
+                .MinimumLength(1)
+                .MaximumLength(20)
+                .NotEmpty()
+                .WithMessage(ValidationMessages.Tag_Description_Length_Error);
+
+            RuleFor(pred => pred.Name)
+                .MinimumLength(1)
+                .MaximumLength(50)
+                .NotEmpty()
+                .WithMessage(ValidationMessages.Tag_Name_Length_Error);
+
+            RuleFor(pred => pred.Id)
+               .NotEmpty()
+               .WithMessage(ValidationMessages.Entity_Required_Id_Error);
+        }
+    }
+}
